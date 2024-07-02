e72717a035a6de60c063163fd446dffd94ed5511
Emily Gong <ehgong@student.unimelb.edu.au>
2024-07-02 17:02:34 +1000

    Added in tailwind css files and components folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import HoverMenu from './HoverMenu';
+import HoverMenu from './components/HoverMenu';
 
 function App() {
   const [image, setImage] = useState(null);
