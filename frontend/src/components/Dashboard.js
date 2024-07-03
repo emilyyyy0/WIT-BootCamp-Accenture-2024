4030e05c051b864c77f916d6b86e6eb27d3c3b97
Emily Gong <ehgong@student.unimelb.edu.au>
2024-07-03 19:39:33 +1000

    Added in dashboard images

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -13,6 +13,44 @@ const Dashboard = () => {
     <div className="flex flex-col items-center justify-start h-screen pt-10">
 
         <h1 className="text-4xl font-bold mb-8">Dashboard</h1>
+        <div style={{ position: 'absolute', top: '100px', left: '100px', display: 'flex', alignItems: 'center', flexDirection: 'row-reverse'}}> 
+          <h2 className="text-5xl font-semibold mb-4">Hello Theodore</h2>
+          <img
+            src="/images/theodore.png" // Path to your home icon image
+            alt="theodre"
+            className="w-25 h-25 ml-2" // Adjusted size and added margin-left
+          />
+        </div>
+
+        <div style={{position: 'absolute', top: '180px', left: '140px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', boxSizing: 'border-box'}}>
+          <h3 className="text-2xl font-semibold text-gray-700 mb-2" style={{ textAlign: 'left' }}>Your veggie patch</h3>
+          <img
+            src="/images/veggie-patch.png" // Path to your home icon image
+            alt="veggie"
+            className="w-70 h-200 ml-2" // Adjusted size and added margin-left
+          />
+        </div>
+
+        <div style={{position: 'absolute', top: '180px', left: '800px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+          <h3 className="text-2xl font-semibold text-gray-700 mb-2" style={{ textAlign: 'left' }}>Your recent viewed lessons</h3>
+          <img
+            src="/images/recent-view.png" // Path to your home icon image
+            alt="recent"
+            className="w-50 h-100 ml-2" // Adjusted size and added margin-left
+          />
+        </div>
+
+        <div style={{position: 'absolute', top: '525px', left: '800px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+          <h3 className="text-2xl font-semibold text-gray-700 mb-2" style={{ textAlign: 'left' }}>This week's work progress</h3>
+          <img
+            src="/images/progress.png" // Path to your home icon image
+            alt="recent"
+            className="w-80 h-100 ml-2" // Adjusted size and added margin-left
+          />
+        </div>
+
+
+        
       
       <div style={{ position: 'absolute', top: '10px', left: '100px' }}> {/* New container for the button */}
         <button
